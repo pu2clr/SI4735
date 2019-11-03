80ee5105ea05b36164f8dd688fdfd3c7715d5d8f
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2019-11-03 14:40:13 -0300

    First Version

--- SI4735.cpp
+++ SI4735.cpp
@@ -3,7 +3,8 @@
  *  Under construction.... Do not use it for while
  */ 
 
-#include "SI4735.h"
+#include <SI4735.h>
+
 
 
 /*
@@ -15,34 +16,50 @@ void SI4735::waitInterrupr(void)
     while (!data_from_si4735) ;
 }
 
+
+
 /*
- * reset
+ * Reset the SI473X   
+ * See Si47XX PROGRAMMING GUIDE; AN332;
  */
 void SI4735::reset()
 {
-    waitToSend();
-    data_from_si4735 = false;
+    pinMode(resetPin, OUTPUT);
+    delay(250);
     digitalWrite(resetPin, LOW);
-    delayMicroseconds(200);
+    delay(250);
     digitalWrite(resetPin, HIGH);
-    delayMicroseconds(200);
-    waitInterrupr();
-    delayMicroseconds(2500);
+    delay(500);
 }
 
+/*
+ * Wait for the si473x is ready (Clear to Send status bit have to be 1).  
+ */
 void SI4735::waitToSend() {
-
-    // TO DO
-    while(false) {};
-   
+    do{
+        delayMicroseconds(2000);
+        Wire.requestFrom(SI473X_ADDR, 0x01);
+    } while (!(Wire.read() & B10000000));
 };
 
 /*
  * Powerup in Analog Mode
  */
-void SI4735::analogPowerUp() {
-    waitToSend();
-    data_from_si4735 = false;
+void SI4735::analogPowerUp(void) {
+
+    reset();
+
+    // Initiate the I2C bus
+    // Wire.begin();
+    delayMicroseconds(1000);
+
+    Wire.beginTransmission(SI473X_ADDR);
+    Wire.write(POWER_UP);
+    Wire.write(powerUp.raw[0]);     // Content of ARG1
+    Wire.write(powerUp.raw[1]);     // COntent of ARG2
+    Wire.endTransmission();
+    // page 12 - Delay at least 500 ms between powerup command and first tune command.
+    delayMicroseconds(550);
 
 }
 
@@ -56,18 +73,29 @@ void SI4735::setBand(byte new_band)
 }
 
 
-
+/*
+ * Gets firmware information 
+ */
 void SI4735::getFirmware( void) {
     //TO DO
 }
 
 
-void SI4735::setup(unsigned int resetPin, unsigned int interruptPin, byte defaultBand)
+/* 
+ * Starts the Si473X device. 
+ * 
+ * @param byte resetPin Digital Arduino Pin used to RESET command 
+ * @param byte interruptPin interrupt Arduino Pin (see your Arduino pinout)
+ * @param byte defaultFunction
+ */ 
+void SI4735::setup(byte resetPin, byte interruptPin, byte defaultFunction)
 {
+    Wire.begin();
+
     this->resetPin = resetPin;
     this->interruptPin = interruptPin;
 
-    // Arduino interrupt setup.
+    // Arduino interrupt setup (you have to know which Arduino Pins can deal with interrupt).
     pinMode(interruptPin, INPUT);
     attachInterrupt(digitalPinToInterrupt(interruptPin), interrupt_hundler, RISING);
 
@@ -76,13 +104,97 @@ void SI4735::setup(unsigned int resetPin, unsigned int interruptPin, byte defaul
 
     data_from_si4735 = false;
 
-    reset();
+    // Set the initial SI473X behavior
+    // CTSIEN   1 -> Interrupt anabled;
+    // GPO2OEN  1 -> GPO2 Output Enable;
+    // PATCH    0 -> Boot normally;
+    // XOSCEN   1 -> Use external crystal oscillator;
+    // FUNC     defaultFunction = 0 = FM Receive; 1 = AM (LW/MW/SW) Receiver.
+    // OPMODE   SI473X_ANALOG_AUDIO = 00000101 = Analog audio outputs (LOUT/ROUT).
 
-    // FM is the default BAND
-    // See pages 17 and 18 (Table 8. Pre-defined Band Table) for more details
-    setBand(defaultBand);
+    // Serial.print("defaultFunction: ");
+    // Serial.print(defaultFunction);
+        
+    setPowerUp(1, 1, 0, 1, defaultFunction, SI473X_ANALOG_AUDIO);
+    // Do Power Up
+    analogPowerUp();
+}
 
-    // You need call it just once.
-    getFirmware();
+/*
+ * Set the Power Up parameters for si473X. 
+ * Use this method to chenge the defaul behavior of the Si473X. Use it before PowerUp()
+ * See See Si47XX PROGRAMMING GUIDE; AN332; pages 65 and 129
+ * 
+ * @param byte CTSIEN sets Interrupt anabled or disabled (1 = anabled and 0 = disabled )
+ * @param byte GPO2OEN sets GP02 Si473X pin enabled (1 = anabled and 0 = disabled )
+ * @param byte PATCH  Used for firmware patch updates. Use it always 0 here. 
+ * @param byte XOSCEN byte XOSCEN set external Crystal enabled or disabled 
+ * @param byte FUNC sets the receiver function have to be used (0 = FM Receive; 1 = AM (LW/MW/SW) Receiver)
+ * @param byte OPMODE set the kind of audio mode you want to use.
+ */
+void SI4735::setPowerUp(byte CTSIEN, byte GPO2OEN, byte PATCH, byte XOSCEN, byte FUNC, byte OPMODE)
+{
+    powerUp.arg.CTSIEN = CTSIEN;              // 1 -> Interrupt anabled;
+    powerUp.arg.GPO2OEN = GPO2OEN;            // 1 -> GPO2 Output Enable;
+    powerUp.arg.PATCH = PATCH;                // 0 -> Boot normally;
+    powerUp.arg.XOSCEN = XOSCEN;              // 1 -> Use external crystal oscillator;
+    powerUp.arg.FUNC = FUNC;                  // 0 = FM Receive; 1 = AM (LW/MW/SW) Receiver.
+    powerUp.arg.OPMODE = OPMODE;              // 0x5 = 00000101 = Analog audio outputs (LOUT/ROUT).
 
+    // Set the current tuning frequancy mode 0X20 = FM and 0x40 = AM (LW/MW/SW)
+    // See See Si47XX PROGRAMMING GUIDE; AN332; pages 55 and 124
+
+ 
+    currentTune = (FUNC == 0)? FM_TUNE_FREQ : AM_TUNE_FREQ;
+}
+
+void SI4735::setFrequency(unsigned freq) {
+
+    waitToSend(); // Wait for the si473x is ready. 
+
+    currentFrequency.value = freq;
+
+    Wire.beginTransmission(SI473X_ADDR);
+    Wire.write(currentTune);
+    Wire.write(0x00);
+    Wire.write(currentFrequency.raw.FREQH);
+    Wire.write(currentFrequency.raw.FREQL);
+    Wire.write(0x00);
+
+    Wire.endTransmission();
+    delayMicroseconds(550);
+}
+
+/*
+ * Look for a station 
+ * See Si47XX PROGRAMMING GUIDE; AN332; page 72
+ * 
+ * @param SEEKUP Seek Up/Down. Determines the direction of the search, either UP = 1, or DOWN = 0. 
+ * @param Wrap/Halt. Determines whether the seek should Wrap = 1, or Halt = 0 when it hits the band limit.
+ */
+void SI4735::seekStation(byte SEEKUP, byte WRAP)
+{
+    si47x_seek seek;
+
+    byte seek_start = (currentTune == FM_TUNE_FREQ)? FM_SEEK_START : AM_SEEK_START;
+
+    seek.arg.SEEKUP = SEEKUP;
+    seek.arg.WRAP = WRAP;
+
+    Wire.beginTransmission(SI473X_ADDR);
+    Wire.write(seek_start); 
+    Wire.write(seek.raw);
+    Wire.endTransmission();
+
+    delayMicroseconds(550);
+}
+
+void SI4735::setAM() {
+    setPowerUp(1, 1, 0, 1, 1, SI473X_ANALOG_AUDIO);
+    analogPowerUp();
+}
+
+void SI4735::setFM() {
+    setPowerUp(1, 1, 0, 1, 0, SI473X_ANALOG_AUDIO);
+    analogPowerUp();
 }
--- SI4735.h
+++ SI4735.h
@@ -13,7 +13,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-
+#define SI473X_ADDR 0x11        // SI473X I2C buss address
 #define POWER_UP 0x01           // Power up device and mode selection.
 #define GET_REV 0x10            // Returns revision information on the device.
 #define POWER_DOWN 0x11         // Power down device.
@@ -21,8 +21,11 @@
 #define GET_PROPERTY 0x13       // Retrieves a property’s value.
 #define GET_INT_STATUS 0x14     // Read interrupt status bits.
 
+#define FM_TUNE_FREQ  0x20
+#define FM_SEEK_START 0x21      // Begins searching for a valid FM frequency.
+
 #define AM_TUNE_FREQ 0x40       // Tunes to a given AM frequency.
-#define AM_SEEK_START 0x41      // Begins searching for a valid frequency.
+#define AM_SEEK_START 0x41      // Begins searching for a valid AM frequency.
 #define AM_TUNE_STATUS 0x42     // Queries the status of the already issued AM_TUNE_FREQ or AM_SEEK_START command.
 #define AM_RSQ_STATUS 0x43      // Queries the status of the Received Signal Quality (RSQ) for the current channel.
 #define AM_AGC_STATUS 0x47      // Queries the current AGC settings.
@@ -30,10 +33,64 @@
 #define GPIO_CTL 0x80           // Configures GPO1, 2, and 3 as output or Hi-Z.
 #define GPIO_SET 0x81           // Sets GPO1, 2, and 3 output level (low or high).
 
-
-
 #define TX_TUNE_FREQ 0x30   // CMD
 #define SET_PROPERTY 0x12   // CMD
+
+
+
+#define SI473X_ANALOG_AUDIO B00000101 // Analog Audio Inputs
+#define SI473X_DIGITAL_AUDIO B00001011 // Digital audio output (DCLK, LOUT/DFS, ROUT/DIO)
+
+/*****************************************************************
+ * SI473X data types 
+ * These data types will be usefull to deal with SI473X 
+ *****************************************************************/
+
+/*
+ * Power Up arguments data type 
+ * See Si47XX PROGRAMMING GUIDE; AN332; pages 64 and 65
+ */ 
+typedef union {
+    struct
+    {
+        // ARG1
+        byte FUNC       : 4;  // Function (0 = FM Receive; 1–14 = Reserved; 15 = Query Library ID)
+        byte XOSCEN     : 1;  // Crystal Oscillator Enable (0 = crystal oscillator disabled; 1 = Use crystal oscillator and and OPMODE=ANALOG AUDIO) .
+        byte PATCH      : 1;  // Patch Enable (0 = Boot normally; 1 = Copy non-volatile memory to RAM).
+        byte GPO2OEN    : 1;  // GPO2 Output Enable (0 = GPO2 output disabled; 1 = GPO2 output enabled).
+        byte CTSIEN     : 1;  // CTS Interrupt Enable (0 = CTS interrupt disabled; 1 = CTS interrupt enabled).
+        // ARG2
+        byte OPMODE; // Application Setting. See page 65
+    } arg;
+    byte raw[2]; // same arg memory position, so same content.
+} si473x_powerup;
+
+/*
+ * Represents how frequency is stored in the si4735.
+ * It helps to convert frequency in unsigned int to two bytes (FREQL and FREQH)  
+ */
+typedef union {
+    struct
+    {
+        byte FREQL; // Tune Frequency High Byte.
+        byte FREQH; // Tune Frequency Low Byte.
+    } raw;
+    unsigned value;
+} si47x_frequency;
+
+/* 
+ *  Represents searching for a valid frequency data type.
+ */
+typedef union {
+    struct
+    {
+        byte RESERVED1 : 2;
+        byte WRAP : 1;   // Determines whether the seek should Wrap = 1, or Halt = 0 when it hits the band limit.
+        byte SEEKUP : 1; // Determines the direction of the search, either UP = 1, or DOWN = 0.
+        byte RESERVED2 : 4;
+    } arg;
+    byte raw;
+} si47x_seek;
 
 volatile static bool data_from_si4735;
 
@@ -45,23 +102,35 @@ static void interrupt_hundler()
 class SI4735
 {
 
-    private:
+        private:
         byte  resetPin;
-        byte  interruptPin; 
+        byte  interruptPin;
+
+        byte currentTune;
+
+        si47x_frequency currentFrequency;
+        si473x_powerup  powerUp;
 
         void reset(void);
         void waitInterrupr(void);
         void waitToSend(void);
 
 
-    public : 
-    
-        void setup(unsigned int, unsigned int, byte);
+    public :
+        void setup(byte resetPin, byte interruptPin, byte defaultFunction);
+        void setPowerUp(byte CTSIEN, byte GPO2OEN, byte PATCH, byte XOSCEN, byte FUNC, byte OPMODE);
+        void analogPowerUp(void);
+        
         void setBand(byte new_band);
         void getFrequency(void);
         void setFrequency(unsigned);
-        void getFirmware(void);
-        
-        void analogPowerUp(void);
+        void setAM(); 
+        void setFM();
 
+
+        void getFirmware(void);
+
+
+        void setFunction(byte FUNC);
+        void seekStation(byte SEEKUP, byte WRAP);
 };
