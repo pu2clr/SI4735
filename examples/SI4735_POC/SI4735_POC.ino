e962f483d25bdd08195dc5f51555119619a9e939
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2019-11-13 17:56:26 -0300

    Examples

--- examples/SI4735_POC/SI4735_POC.ino
+++ examples/SI4735_POC/SI4735_POC.ino
@@ -30,9 +30,10 @@ void setup()
   delay(500);
 
   si4735.setup(RESET_PIN, INTERRUPT_PIN, FM_FUNCTION);
-
+  
   // Starts defaul radio function and band (FM; from 84 to 108 MHz; 103.9 MHz; step 100KHz)
   si4735.setFM(8400, 10800,  10390, 10);
+  delay(1000);
   currentFrequency = previousFrequency = si4735.getFrequency();
   si4735.setVolume(45);
   showStatus();
