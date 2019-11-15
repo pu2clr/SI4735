b38d3a74eeac9c8e4db47b9b3f6df2cad4d170fe
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2019-11-15 14:53:02 -0300

    Examples

--- examples/SI4735_LCD_20x4_I2C/SI4735_LCD_20x4_I2C.ino
+++ examples/SI4735_LCD_20x4_I2C/SI4735_LCD_20x4_I2C.ino
@@ -6,6 +6,8 @@
 
 #include <SI4735.h>
 
+#include <LiquidCrystal_I2C.h>
+
 #define RESET_PIN 12
 
 #define AM_FUNCTION 1
@@ -15,26 +17,44 @@
 
 unsigned currentFrequency;
 unsigned previousFrequency;
+byte rssi = 0;
 
 SI4735 si4735;
+LiquidCrystal_I2C lcd(0x27, 20, 4);  // please check the address of you I2C device
 
 void setup()
 {
+
   Serial.begin(9600);
-  Serial.println("Test and validation of the SI4735 Arduino Library.");
-  Serial.println("AM and FM station tuning test.");
+
+  
+  lcd.init();
+
+  lcd.backlight();
+  lcd.setCursor(6, 0);
+  lcd.print("SI4735");
+  lcd.setCursor(2, 1);
+  lcd.print("Arduino Library");
+  lcd.setCursor(0, 3);
+  lcd.print("By PU2CLR - Ricardo");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("FM and AM (MW & SW)");
+  lcd.setCursor(4, 2);
+  lcd.print("Tuning Test");
 
   showHelp();
 
   delay(500);
 
   si4735.setup(RESET_PIN, FM_FUNCTION);
-  
+
   // Starts defaul radio function and band (FM; from 84 to 108 MHz; 103.9 MHz; step 100KHz)
   si4735.setFM(8400, 10800,  10390, 10);
-  
+
   delay(500);
-  
+
   currentFrequency = previousFrequency = si4735.getFrequency();
   si4735.setVolume(45);
   showStatus();
@@ -42,37 +62,46 @@ void setup()
 
 
 void showHelp() {
-  Serial.println("Type F to FM; A to MW; L to LW; and 1 to SW");
-  Serial.println("Type U to increase and D to decrease the frequency");
-  Serial.println("Type S or s to seek station Up or Down");
-  Serial.println("Type + or - to volume Up or Down");
-  Serial.println("Type 0 to show current status");  
-  Serial.println("Type ? to this help.");
-  Serial.println("==================================================");
+
   delay(1000);
 }
 
 // Show current frequency
-void showStatus()
-{
-  
-  Serial.print("You are tuned on ");
+void showStatus() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
   if (si4735.isCurrentTuneFM() ) {
-    Serial.print(String(currentFrequency / 100.0, 2));
-    Serial.print("MHz ");
-    Serial.print((si4735.getCurrentPilot())?"STEREO":"MONO");
+    lcd.print("FM ");
+    lcd.setCursor(7, 0);
+    lcd.print(String(currentFrequency / 100.0, 2));
+    lcd.setCursor(17, 0);
+    lcd.print("MHz ");
+    lcd.setCursor(0,3);
+    lcd.print((si4735.getCurrentPilot())?"STEREO":"MONO");
   } else {
-    Serial.print(currentFrequency);
-    Serial.print("KHz");
+    lcd.print("AM ");
+    lcd.setCursor(8, 0);    
+    lcd.print(currentFrequency);
+    lcd.setCursor(17, 0);
+    lcd.print("KHz");
   }
-  Serial.print(" [SNR:" );
-  Serial.print(si4735.getCurrentSNR());
-  Serial.print("dB");
-    
-  Serial.print(" Signal:" );
-  Serial.print(si4735.getCurrentRSSI());
-  Serial.println("dBuV]");
-  
+  // lcd.setCursor(10,3);
+  // lcd.print("S:");
+  // lcd.setCursor(13,3);
+  // lcd.print(si4735.getCurrentRSSI());
+  lcd.setCursor(16,3);
+  lcd.print("dBuV");
+  delay(300);
+
+}
+
+
+void showRSSI() {
+  lcd.setCursor(10,3);
+  lcd.print("S:");
+  lcd.setCursor(13,3);
+  lcd.print(si4735.getCurrentRSSI());
+  delay(300);
 }
 
 // Main
@@ -97,9 +126,9 @@ void loop()
       case 'F':
         si4735.setFM(8600, 10800,  10390, 10);
         break;
-      case '1': 
-           si4735.setAM(9400, 9990,  9600, 5); 
-           break;
+      case '1':
+        si4735.setAM(9400, 9990,  9600, 5);
+        break;
       case 'U':
       case 'u':
         si4735.frequencyUp();
@@ -116,7 +145,7 @@ void loop()
         break;
       case '0':
         showStatus();
-        break;  
+        break;
       case '?':
         showHelp();
         break;
@@ -124,11 +153,16 @@ void loop()
         break;
     }
   }
-  delay(100);
+  delay(50);
   currentFrequency = si4735.getFrequency();
   if ( currentFrequency != previousFrequency ) {
     previousFrequency = currentFrequency;
     showStatus();
-    delay(300);
   }
+  
+  if ( rssi != si4735.getCurrentRSSI() ) {
+    rssi = si4735.getCurrentRSSI();
+    showRSSI();   
+  }
+  
 }
