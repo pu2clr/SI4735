39770c6da0b14e6ed4f8e510ead3e2b3b97fee1d
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2019-11-29 14:32:39 -0300

    Examples

--- examples/SI4735_99_TESTS/SI4735_03_OLED_I2C_SSB/SI4735_03_OLED_I2C_SSB.ino
+++ examples/SI4735_99_TESTS/SI4735_03_OLED_I2C_SSB/SI4735_03_OLED_I2C_SSB.ino
@@ -1,26 +1,26 @@
 /*
- * This sketch applies patches on Si4735 devices.
- * In this context, a patch is a piece of software used to change the behavior of the SI4735 device.
- *
- * There is little information available about patching the SI4735. The following information is the understanding
- * of the author.
- * 
- * A SI4735 patches are executed internally (run by internal MCU) of the device. Usually, patches are  used to fixes bugs
- * or add improvements and  new features of the firmware installed in the internal ROM of the device.
- * Patches to the SI4735 are distributed in binary form and have to be transferred to the internal RAM of the device
- * by the host MCU (in this case Arduino).
- * Since the RAM is volatile memory, the patch stored into the device gets lost when you turn off the system.
- * Consequently, the content of the patch has to be transferred again to the device each time after turn on the
- * system or reset the device.
- * 
- * ATTENTION:
- * The author of this sketch does not guarantee that this procedure will work in your development environment.
- * Given this, it is at your own risk to continue with the procedures suggested here.
- * This sketch works with the I2C communication protocol and is designed to apply a SSB extension PATCH to CI
- * SI4735-D60 every time you switch to SSB mode. Once again, the author disclaims any liability for any damage 
- * this procedure may cause to your SI4735 or other devices that you are using. 
- * 
- * By Ricardo Lima Caratti, Nov 2019
+   This sketch applies patches on Si4735 devices.
+   In this context, a patch is a piece of software used to change the behavior of the SI4735 device.
+
+   There is little information available about patching the SI4735. The following information is the understanding
+   of the author.
+
+   A SI4735 patches are executed internally (run by internal MCU) of the device. Usually, patches are  used to fixes bugs
+   or add improvements and  new features of the firmware installed in the internal ROM of the device.
+   Patches to the SI4735 are distributed in binary form and have to be transferred to the internal RAM of the device
+   by the host MCU (in this case Arduino).
+   Since the RAM is volatile memory, the patch stored into the device gets lost when you turn off the system.
+   Consequently, the content of the patch has to be transferred again to the device each time after turn on the
+   system or reset the device.
+
+   ATTENTION:
+   The author of this sketch does not guarantee that this procedure will work in your development environment.
+   Given this, it is at your own risk to continue with the procedures suggested here.
+   This sketch works with the I2C communication protocol and is designed to apply a SSB extension PATCH to CI
+   SI4735-D60 every time you switch to SSB mode. Once again, the author disclaims any liability for any damage
+   this procedure may cause to your SI4735 or other devices that you are using.
+
+   By Ricardo Lima Caratti, Nov 2019
 */
 
 #include <SI4735.h>
@@ -42,20 +42,20 @@
 #define ENCODER_PIN_B 2
 
 // Button controllers
-#define VOL_UP 6              // Volume Volume Up
-#define VOL_DOWN 7            // Volume Down
-#define BANDWIDTH_BUTTON 5    // Next Band
-#define BAND_BUTTON_UP 8      // Band Up
-#define BAND_BUTTON_DOWN 9    // Band Down
-#define MODE_SWITCH_BUTTON 10 // Switch AM/SSB
-#define FM_SWITCH_BUTTON 11   // Switch FM / (AM/SSB)
+#define BANDWIDTH_BUTTON 4       // Next Band
+#define VOL_UP 5                 // Volume Volume Up
+#define VOL_DOWN 6               // Volume Down
+#define BAND_BUTTON_UP 7         // Band Up
+#define BAND_BUTTON_DOWN 8       // Band Down
+#define SSB_SWITCH_BUTTON 9      // Switch AM/SSB
+#define LSB_SSB_SWITCH_BUTTON 10 // Switch LSB/USB
+#define FM_SWITCH_BUTTON 11      // Switch FM / (AM/SSB)
 
 #define MIN_ELAPSED_TIME 100
 
 #define MODE_AM 0
 #define MODE_LSB 1
 #define MODE_USB 2
-
 
 const int size_content_full = sizeof ssb_patch_content_full;
 
@@ -71,34 +71,33 @@ unsigned previousFrequency;
 byte bandwidthIdx = 1;
 char *bandwitdth[] = {"6", "4", "3", "2", "1", "1.8", "2.5"};
 
-
-typedef struct {
+typedef struct
+{
   unsigned minimumFreq; // Minimum frequency to the band
   unsigned maximumFreq; // Maximum frequency to the band
-  unsigned currentFreq; // Used to store the current band before you switch to another band 
-  unsigned currentStep; // Used to store the corrent step 
-  unsigned defaultMode; // MODE_AM, MODE_LSB e MODE_USB
+  unsigned currentFreq; // Used to store the current band before you switch to another band
+  byte currentStep;     // Used to store the corrent step
+  byte currentMode;     // MODE_AM, MODE_LSB e MODE_USB
 } Band;
 
 // SW band table
 Band band[] = {
-  {4600, 5200, 4850, 5},
-  {5700, 6200, 6000, 5},
-  {7000, 7500, 7100, 5},
-  {9300, 10000, 9600, 5},
-  {11400, 12200, 11940, 5},
-  {13500, 13900, 13600, 5},
-  {14000} 14400, 14200, 1}, // 20 meters
-  {15000, 15800, 15400, 5},
-  {17400, 17900, 17600, 5},
-  {21400, 21800, 21500, 5},
-  {27000, 27500, 27220, 1},
-  {28000, 28500, 28400, 1}
-};
+    {3500, 4000, 3700, 1, MODE_LSB},
+    {4600, 5200, 4850, 5, MODE_AM},
+    {5700, 6200, 6000, 5, MODE_AM},
+    {7000, 7300, 7100, 5, MODE_AM},
+    {9300, 10000, 9600, 5, MODE_AM},
+    {11400, 12200, 11940, 5, MODE_AM},
+    {13500, 13900, 13600, 5, MODE_AM},
+    {14000, 14400, 14100, 1, MODE_USB},
+    {15000, 15800, 15400, 5, MODE_AM},
+    {17400, 19900, 17600, 5, MODE_AM},
+    {21000, 21800, 21100, 1, MODE_USB},
+    {27000, 27500, 27700, 1, MODE_USB},
+    {28000, 28500, 28400, 1, MODE_USB}};
 
 const int lastBand = (sizeof band / sizeof(Band)) - 1;
-int  currentFreqIdx = 2; // 41M
-
+int currentFreqIdx = 2; // 41M
 
 byte rssi = 0;
 byte stereo = 1;
@@ -133,7 +132,10 @@ void setup()
   display.setCursor(0, 0);
   display.print("Si4735 Arduino Library");
   delay(500);
-  display.setCursor(30, 3);
+  display.setCursor(0, 3);
+  display.print("FM / AM / SSB Test");
+  delay(500);
+  display.setCursor(30, 6);
   display.print("By PU2CLR");
   delay(3000);
   display.clear();
@@ -146,7 +148,7 @@ void setup()
   si4735.setup(RESET_PIN, AM_FUNCTION);
 
   si4735.setTuneFrequencyAntennaCapacitor(1); // Set antenna tuning capacitor for SW.
-  
+
   si4735.setAM(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep);
 
   currentFrequency = previousFrequency = si4735.getFrequency();
@@ -240,29 +242,34 @@ void showVolume()
   display.print(volume);
 }
 
+void bandUp()
+{
 
-void bandUp() {
-  
   // save the current frequency for the band
   band[currentFreqIdx].currentFreq = currentFrequency;
-  if ( currentFreqIdx < lastBand ) {
+  if (currentFreqIdx < lastBand)
+  {
     currentFreqIdx++;
-  } else {
+  }
+  else
+  {
     currentFreqIdx = 0;
   }
 
   si4735.setTuneFrequencyAntennaCapacitor(1); // Set antenna tuning capacitor for SW.
   si4735.setAM(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep);
-
-
 }
 
-void bandDown() {
+void bandDown()
+{
   // save the current frequency for the band
   band[currentFreqIdx].currentFreq = currentFrequency;
-  if ( currentFreqIdx > 0 ) {
+  if (currentFreqIdx > 0)
+  {
     currentFreqIdx--;
-  } else {
+  }
+  else
+  {
     currentFreqIdx = lastBand;
   }
   si4735.setTuneFrequencyAntennaCapacitor(1); // Set antenna tuning capacitor for SW.
@@ -270,17 +277,17 @@ void bandDown() {
 }
 
 /*
- * Patches to the SI4735 are transferred to the internal RAM of the device.
- * All content of the patch will get lost when you switch to another mode like AM or FM. 
- * So, the content of the patch has to be transferred again to the device each time you select
- * SSB mode.
- */
-void loadSSB() {
+   Patches to the SI4735 are transferred to the internal RAM of the device.
+   All content of the patch will get lost when you switch to another mode like AM or FM.
+   So, the content of the patch has to be transferred again to the device each time you select
+   SSB mode.
+*/
+void loadSSB()
+{
   si4735.queryLibraryId(); // Is it really necessary here? I will check it.
   si4735.patchPowerUp();
   si4735.downloadPatch(ssb_patch_content_full, size_content_full);
 }
-
 
 /*
    Main
@@ -291,7 +298,6 @@ void loop()
   // Check if the encoder has moved.
   if (encoderCount != 0)
   {
-
     if (encoderCount == 1)
       si4735.frequencyUp();
     else
@@ -301,30 +307,48 @@ void loop()
   }
 
   // Check button commands
-  if (digitalRead(BANDWIDTH_BUTTON) | digitalRead(BAND_BUTTON_UP) | digitalRead(BAND_BUTTON_DOWN) | digitalRead(VOL_UP) | digitalRead(VOL_DOWN))
+  if (digitalRead(BANDWIDTH_BUTTON) | digitalRead(BAND_BUTTON_UP) | digitalRead(BAND_BUTTON_DOWN) | digitalRead(VOL_UP) |
+      digitalRead(VOL_DOWN) | digitalRead(SSB_SWITCH_BUTTON) | digitalRead(FM_SWITCH_BUTTON))
   {
-
-    // check if some button is pressed
-    if (digitalRead(BANDWIDTH_BUTTON) == HIGH && (millis() - elapsedButton) > MIN_ELAPSED_TIME)
+    // check if which button was pressed
+    if ((millis() - elapsedButton) > MIN_ELAPSED_TIME)
     {
-      bandwidthIdx++;
-      if (bandwidthIdx > 6)  bandwidthIdx = 0;
+      if (digitalRead(FM_SWITCH_BUTTON) == HIGH)
+      {
+        if (si4735.isCurrentTuneFM())
+          si4735.setAM(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep);
+        else
+          si4735.setFM();
+      }
+      else if (digitalRead(SSB_SWITCH_BUTTON) == HIGH)
+      {
+        loadSSB();
+        si4735.setSSB(band[currentFreqIdx].currentMode);
+      }
+      else if (digitalRead(LSB_SSB_SWITCH_BUTTON) == HIGH)
+      {
+        band[currentFreqIdx].currentMode = (band[currentFreqIdx].currentMode == MODE_LSB) ? MODE_USB : MODE_LSB;
+      }
+      else if (digitalRead(BANDWIDTH_BUTTON) == HIGH)
+      {
+        bandwidthIdx++;
+        if (bandwidthIdx > 6)
+          bandwidthIdx = 0;
+        si4735.setBandwidth(bandwidthIdx, 0);
+        showStatus();
+      }
+      else if (digitalRead(BAND_BUTTON_UP) == HIGH)
+        bandUp();
+      else if (digitalRead(BAND_BUTTON_DOWN) == HIGH)
+        bandDown();
+      else if (digitalRead(VOL_UP) == HIGH)
+        si4735.volumeUp();
+      else if (digitalRead(VOL_DOWN) == HIGH)
+        si4735.volumeDown();
 
-      si4735.setBandwidth(bandwidthIdx, 0);
-      showStatus();
+      elapsedButton = millis();
     }
-    else if (digitalRead(BAND_BUTTON_UP) == HIGH && (millis() - elapsedButton) > MIN_ELAPSED_TIME)
-      bandUp();
-    else if (digitalRead(BAND_BUTTON_DOWN) == HIGH && (millis() - elapsedButton) > MIN_ELAPSED_TIME)
-      bandDown();
-    else if (digitalRead(VOL_UP) == HIGH && (millis() - elapsedButton) > MIN_ELAPSED_TIME)
-      si4735.volumeUp();
-    else if (digitalRead(VOL_DOWN) == HIGH && (millis() - elapsedButton) > MIN_ELAPSED_TIME)
-      si4735.volumeDown();
-
-    elapsedButton = millis();
   }
-
 
   // Show the current frequency only if it has changed
   currentFrequency = si4735.getFrequency();
