1e2d5a8f56ee582ca45b4efdc5951d50c279b594
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2020-01-19 10:52:42 -0300

    Extras

--- examples/SI4735_06_POC_SSB/SI4735_06_POC_SSB.ino
+++ examples/SI4735_06_POC_SSB/SI4735_06_POC_SSB.ino
@@ -130,7 +130,7 @@ void setup()
   si4735.setSSB(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep, band[currentFreqIdx].currentSSB);
   delay(100);
   currentFrequency = si4735.getFrequency();
-  si4735.setVolume(40);
+  si4735.setVolume(60);
   showHelp();
   showStatus();
 }
