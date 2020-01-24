e0c4ec7abbaf5902420fa8b9b48cc8a50e3787b0
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2020-01-24 19:52:13 -0300

    Examples

--- examples/SI4735_99_TESTS/SI4735_ATTINY85_POC/SI4735_ATTINY85_POC.ino
+++ examples/SI4735_99_TESTS/SI4735_ATTINY85_POC/SI4735_ATTINY85_POC.ino
@@ -10,6 +10,8 @@
 */
 
 #include <SI4735.h>
+#include <LiquidCrystal_I2C.h>
+
 
 #define RESET_PIN 3
 
@@ -26,6 +28,8 @@ uint16_t lastFmFrequency = 10390;   // Starts FM on 103,9MHz
 
 SI4735 si4735;
 
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+
 void setup()
 {
 
@@ -33,14 +37,48 @@ void setup()
   pinMode(SEEK_BUTTON_UP, INPUT_PULLUP);
   pinMode(SEEK_BUTTON_DOWN, INPUT_PULLUP);
 
+
+  lcd.init();
+
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Si4735-Attiny85");
+  lcd.setCursor(0, 1);
+  lcd.print("   By PU2CLR   ");
+ 
   si4735.setup(RESET_PIN, FM_FUNCTION);
   // Starts defaul radio function and band (FM; from 84 to 108 MHz; 103.9 MHz; step 100KHz)
   si4735.setFM(8400, 10800, 10390, 10);
-  delay(250);
+  delay(100);
   currentFrequency = si4735.getFrequency();
   si4735.setVolume(55);
+  delay(100);
+  lcd.clear();
+  showStatus();
 }
 
+
+void showStatus() {
+  // Clear just the frequency information space.
+  lcd.setCursor(5,0); 
+  lcd.print("        ");
+  
+  lcd.setCursor(0, 0);
+  if (si4735.isCurrentTuneFM() ) {
+    lcd.print("FM ");
+    lcd.setCursor(5, 0);
+    lcd.print(currentFrequency / 100.0);
+    lcd.setCursor(13, 0);
+    lcd.print("MHz");
+  } else {
+    lcd.print("AM ");
+    lcd.setCursor(5, 0);
+    lcd.print(currentFrequency);
+    lcd.setCursor(13, 0);
+    lcd.print("KHz");
+  }
+}
 
 
 // Main
@@ -55,15 +93,16 @@ void loop()
       lastAmFrequency = currentFrequency;
       si4735.setFM(8600, 10800,  lastFmFrequency, 10);
     }
+    showStatus();
   } else if (digitalRead(SEEK_BUTTON_UP) == LOW ) {
     si4735.seekStationUp();
-    delay(250);
     currentFrequency = si4735.getFrequency();
+    showStatus();
   }
   else if (digitalRead(SEEK_BUTTON_DOWN) == LOW ) {
     si4735.seekStationDown();
-    delay(250);
     currentFrequency = si4735.getFrequency();
+    showStatus();
   }
-  delay(100);
+  delay(10);
 }
