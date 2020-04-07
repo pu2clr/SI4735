17b4fa124938854bb85f93041506b612005ece54
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2020-04-07 01:33:50 -0300

    Si47XX Arduino Library

--- SI4735.cpp
+++ SI4735.cpp
@@ -637,7 +637,7 @@ void SI4735::setAM(uint16_t fromFreq, uint16_t toFreq, uint16_t initialFreq, uin
  * si4735.setFM(6400, 10800, 10390, 10);
  * @endcode
  * 
- * @example ./examples/SI4735_01_POC/SI4735_01_POC.ino
+ * @example ./examples/SI47XX_01_SERIAL_MONITOR/SI4735_01_POC.ino
  * 
  * @see Si47XX PROGRAMMING GUIDE; AN332; pages 70
  * @see setFM()
