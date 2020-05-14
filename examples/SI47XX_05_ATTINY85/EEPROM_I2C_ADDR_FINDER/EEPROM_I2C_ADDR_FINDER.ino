642c4162977aec779da10c3c634cea12aadc3856
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2020-05-14 14:41:12 -0300

    Examples

---
+++ examples/SI47XX_05_ATTINY85/EEPROM_I2C_ADDR_FINDER/EEPROM_I2C_ADDR_FINDER.ino
@@ -0,0 +1,52 @@
+/*
+ * This sketch is useful to find the EEPROM device I2C bus address. 
+ * Use the Serial Monitor.
+ */
+
+#include <Wire.h>
+ 
+void setup() {
+
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
+  delay(300);
+  digitalWrite(12, HIGH);
+  delay(300);
+  
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("\nI2C Scanner");
+}
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+  delay(5000);          
+}
--- examples/SI47XX_09_TOOLS/SI47XX_09_SAVE_SSB_PATHC_EEPROM/SI47XX_09_SAVE_SSB_PATHC_EEPROM.ino
+++ examples/SI47XX_09_TOOLS/SI47XX_09_SAVE_SSB_PATHC_EEPROM/SI47XX_09_SAVE_SSB_PATHC_EEPROM.ino
@@ -1,20 +1,24 @@
 /*
-  This sketch stores the SI4735 SSB patch content on an EEPROM. 
+  This sketch stores the SI4735 SSB patch content on an EEPROM.
   It might useful for Board or MCU with few memory.
+  The Idea is saving memory of your MCU by storing a SI47XX patche in an external memory (EEPROM)
 
   Under construction....
-  
-  Ricardo Lima Caratti 2020    
+
+  Ricardo Lima Caratti 2020
 */
 
 // under construction ...
 #include <Wire.h>
+
 #include "patch_init.h" // SSB patch for whole SSBRX initialization string
 
 const uint16_t size_content = sizeof ssb_patch_content; // see ssb_patch_content in patch_full.h or patch_init.h
 
 //defines the EEPROM I2C addresss.
 #define EEPROM_I2C_ADDR 0x50 // You might need to change this value
+
+#define EEPROM_OFFSET 10     // START WRITING in eeprom 
 
 char buffer(80);
 
@@ -24,82 +28,83 @@ typedef union {
     uint8_t lowByte;
     uint8_t highByte;
   } raw;
+  uint8_t  byteArray[2];
   uint16_t value;
-} bytes_to_word16; 
+} bytes_to_word16;
 
 
-// Interger to bytes representation 
+// Interger to bytes representation
 bytes_to_word16 size_value;
-
-const uint8_t content_id[] = "SI4735-D60-init";
-// const uint8_t content_id[] = "SI4735-D60-full";
-const uint16_t size_id = sizeof content_id;
+const uint8_t content_id[] =    "SI4735-D60-init-"; // 16 bytes
+// const uint8_t content_id[] = "SI4735-D60-full-"; // 16 bytes
+const uint16_t size_id = sizeof content_id -1 ;
 
 void setup()
 {
   Serial.begin(9600);
-  while(!Serial);
+  while (!Serial);
 
   showMsg("Storing the patch file..");
+
   showMsgValue("Size of patch ID.....: %u bytes.", size_id);
   showMsgValue("Size of patch content: %u bytes.", size_content);
   uint32_t t1 =  millis();
   eepromWritePatch();
+  // clearEeprom();
   uint32_t t2 = millis();
-  showMsgValue("Finish! Elapsed time: %ul milliseconds.", t2-t1);
+  showMsgValue("Finish! Elapsed time: %ul milliseconds.", t2 - t1);
+
+  showMsg("Checking the values stored");
+  checkPatch();
+
 
 }
 
 
 void showMsg(const char *msg) {
-  Serial.println(msg); 
+  Serial.println(msg);
 }
 
 void showMsgValue(const char *msg, uint16_t value) {
   char buffer[80];
-  sprintf(buffer,msg,value);
+  sprintf(buffer, msg, value);
   Serial.println(buffer);
 }
 
 
-/**
- * Writes an identification that will be check by the load SSB patch process 
- * 
- */
-void eepromWriteId() {
-     eepromWriteBlock(EEPROM_I2C_ADDR,0,content_id,size_id);
-}
 
-void eepromWriteContentSize() {
-    
-}
-
-/**
- * Writes the patch 
- */
 void eepromWritePatch() {
-  eepromWriteId();
-  // eepromWriteBlock(EEPROM_I2C_ADDR, size_id, ssb_patch_content, size_content);
-} 
 
+  bytes_to_word16 size_patch;
+  int nBlk =  size_content / 8;
+  int nOffset;
+  uint8_t content[8];
 
-/*
- * Writes a data (byte) to the eepro at a given position (offset).
- * @param i2c_address I2C bus address of the eeprom device 
- * @param offset address (position) where the data will be saved;
- * @param data  data to be saved 
- */
-void eepromWrite(uint8_t i2c_address, uint16_t offset, uint8_t data)
-{
-  bytes_to_word16 eeprom;
-  eeprom.value = offset;
-  Wire.beginTransmission(i2c_address);
-  // First, you have to tell where you want to save the data (offset is the position).
-  Wire.write(eeprom.raw.highByte); // Most significant Byte
-  Wire.write(eeprom.raw.lowByte);  // Less significant Byte
-  Wire.write(data);                // Writes the data at the right position (offset)
-  Wire.endTransmission();
-  delay(5);
+  // Stores the identification of the patch
+  eepromWriteBlock(EEPROM_I2C_ADDR, EEPROM_OFFSET + 0, &content_id[0], 8); // Writes the first 8 bytes of the identification
+  eepromWriteBlock(EEPROM_I2C_ADDR, EEPROM_OFFSET + 8, &content_id[8], 8); // Writes the last  8 bytes of the identification
+
+  // Stores the patch size (unsigned integer - 16 bits)
+  size_patch.value = size_content;
+  
+  eepromWriteBlock(EEPROM_I2C_ADDR, EEPROM_OFFSET + 16, size_patch.byteArray, 2); // Writes the size of the patch (integer value)
+  nOffset = EEPROM_OFFSET + 16 + 2;
+
+  // Stores the patch content
+  for (int i = 0; i < nBlk; i++ ) {
+    for (int k = 0; k < 8; k++ )  {
+      content[k] = pgm_read_byte_near(ssb_patch_content + (i * 8) + k);
+    }
+
+    /*
+      if ( nOffset < 64 ) {
+      for (int k = 0; k < 8; k++ ) Serial.println(content[k],HEX);
+      } */
+
+    eepromWriteBlock(EEPROM_I2C_ADDR, nOffset, content, 8);
+    nOffset += 8;
+  }
+
 }
 
 void  eepromWriteBlock(uint8_t i2c_address, uint16_t offset, uint8_t const * pData, uint8_t blockSize)
@@ -113,36 +118,99 @@ void  eepromWriteBlock(uint8_t i2c_address, uint16_t offset, uint8_t const * pDa
 
   for (int i = 0; i < blockSize; i++)
     Wire.write(*pData++); //dispatch the data bytes
- 
+
   Wire.endTransmission();
-  delay(5); 
+  delay(5);
 
 }
 
-/*
- * Reads a data (byte) from the eepro at a given position (offset).
- * @param i2c_address I2C bus address of the eeprom device 
- * @param offset address (position) where the data will be saved;
- * @return data (byte)
- */
-byte readEEPROM(uint8_t i2c_address, uint16_t offset)
-{
+
+void checkPatch() {
+
   bytes_to_word16 eeprom;
-  eeprom.value = offset;
-  Wire.beginTransmission(i2c_address);
+  bytes_to_word16 patch_size;
+
+  delay(500);
+
+
+  // Shows the patch identification stored in the eeprom
+  Serial.print("\nStored Patch ID.........: ");
+  Wire.beginTransmission(EEPROM_I2C_ADDR);
+  Wire.write(0x00);
+  Wire.write(EEPROM_OFFSET);
+  Wire.endTransmission();
+  delay(5);
+  // Shows the patch identification.
+  Wire.requestFrom(EEPROM_I2C_ADDR, 16);
+  for ( int k = 0; k < 16; k++)  {
+    uint8_t c = Wire.read();
+    Serial.write(c);
+  }
+  delay(5);
+
+  // Shows the size of the patch stored in the eeprom.
+  Serial.print("\nStored patch size.......: ");
+  eeprom.value = EEPROM_OFFSET + 16;
+  Wire.beginTransmission(EEPROM_I2C_ADDR);
   Wire.write(eeprom.raw.highByte); // Most significant Byte
   Wire.write(eeprom.raw.lowByte);  // Less significant Byte
   Wire.endTransmission();
-  Wire.requestFrom((int) i2c_address, (int) 1);
-  if (Wire.available())
-    return Wire.read();
-  else
-    return 0xFF;
+  delay(5);
+  Wire.requestFrom(EEPROM_I2C_ADDR, 2);
+  patch_size.byteArray[0] = Wire.read();
+  patch_size.byteArray[1] = Wire.read();
+  Serial.print(patch_size.value);
+
+  Serial.println("\nReading Patch Content sample......");
+  Serial.println("\nFirst 8 patch commands sample.");
+
+  dumpEeprom(EEPROM_OFFSET + 16 + 2, 8);
+
+  Serial.println("\nLast 8 patch commands sample.");
+  dumpEeprom(EEPROM_OFFSET + 16 + 2 + size_content - 64, 8);
+
 }
 
-// Erase all eeprom content
-void eepromClear()
+
+void dumpEeprom(uint16_t offset, int sample) {
+
+  bytes_to_word16 eeprom;
+
+  delay(500);
+
+  eeprom.value = offset;
+
+  for ( int i = 0; i < sample; i++) {
+
+    Wire.beginTransmission(EEPROM_I2C_ADDR);
+    Wire.write(eeprom.raw.highByte); // offset Most significant Byte
+    Wire.write(eeprom.raw.lowByte);  // offset Less significant Byte
+    Wire.endTransmission();
+    delay(5);
+
+    Wire.requestFrom(EEPROM_I2C_ADDR, 8);
+    for ( int k = 0; k < sample; k++) {
+      uint8_t c = Wire.read();
+      Serial.print(c, HEX); // SHows patch id
+      Serial.print(" ");
+    }
+    Serial.print("\n");
+    eeprom.value += 8;
+    delay(5);
+  }
+
+}
+
+
+
+// Erases all eeprom content
+void clearEeprom()
 {
+  uint8_t content[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  for ( int i = 0; i < 16384 ; i += 8 ) {
+    Wire.beginTransmission(EEPROM_I2C_ADDR);
+    eepromWriteBlock(EEPROM_I2C_ADDR, i, content, 8);
+  }
 }
 
 void loop()
