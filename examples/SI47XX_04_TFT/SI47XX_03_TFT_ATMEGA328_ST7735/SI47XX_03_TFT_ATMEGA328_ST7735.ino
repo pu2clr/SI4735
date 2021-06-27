e652bd21fc469da40f37339a550b7c4da516b5ee
pu2clr <pu2clr@gmail.com>
2021-06-27 18:02:27 -0300

    SI473X Arduino Library

--- examples/SI47XX_04_TFT/SI47XX_03_TFT_ATMEGA328_ST7735/SI47XX_03_TFT_ATMEGA328_ST7735.ino
+++ examples/SI47XX_04_TFT/SI47XX_03_TFT_ATMEGA328_ST7735/SI47XX_03_TFT_ATMEGA328_ST7735.ino
@@ -273,6 +273,8 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A), rotaryEncoder, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_B), rotaryEncoder, CHANGE);
 
+  rx.getDeviceI2CAddress(RESET_PIN);
+
   // rx.setup(RESET_PIN, 1); // Starts FM mode and ANALOG audio mode
   rx.setup(RESET_PIN, 0, 1, SI473X_ANALOG_AUDIO); // Starts FM mode and ANALOG audio mode.
  
