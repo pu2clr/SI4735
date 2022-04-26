4cbecf017d4a46f001718147bc6a1585d6e23aff
pu2clr <pu2clr@gmail.com>
2022-04-26 09:21:49 -0300

    SI473X Arduino Library

--- examples/SI47XX_99_AUTO_BANDPASS_FILTER/SI473X_01_POC_SERIAL_MONITOR/SI473X_01_POC_SERIAL_MONITOR.ino
+++ examples/SI47XX_99_AUTO_BANDPASS_FILTER/SI473X_01_POC_SERIAL_MONITOR/SI473X_01_POC_SERIAL_MONITOR.ino
@@ -16,63 +16,62 @@
 */
 
 #include <SI4735.h>
-#include "AutoBPF.h"
-
+#include "AutoBPF.h" // Small Arduino Library to control the BPF device. See https://github.com/pu2clr/auto_bpf_arduino
 
 #define RESET_PIN 12
 
 #define AM_FUNCTION 1
-#define AUDIO_MUTE 1   
+#define AUDIO_MUTE 1
 
-
-
-#define FILTER_PIN1   14
-#define FILTER_PIN2   15
+#define FILTER_PIN1 14
+#define FILTER_PIN2 15
 
 typedef struct
 {
-  const char *freqName;
-  uint16_t minimumFreq;
-  uint16_t maximumFreq;
-  uint16_t currentFreq;
-  uint16_t currentStep;
+  const char *freqName; // Band Name
+  uint16_t minimumFreq; // Initial frequency of the Band
+  uint16_t maximumFreq; // Final frequency of the Band
+  uint16_t currentFreq; // Default Frequency. It also stores the current frequency during operation.
+  uint16_t currentStep; // Default step of the band
+  uint8_t filter;       // Filter number used by the band (0 - 3)
 } Band;
 
-Band band[] = {{" MW", 520,  1700, 810, 10},
-               {"60m", 4700, 5200, 4850, 5},
-               {"49m", 5700, 6200, 6000, 5},
-               {"41m", 7100, 7600, 7300, 5},
-               {"31m", 9300, 10000, 9600, 5},
-               {"25m", 11400, 12200, 11940, 5},
-               {"22m", 13500, 13900, 13740, 5},
-               {"19m", 15000, 15800, 15300, 5},
-               {"16m", 17400, 17900, 17850, 5},
-               {"13m", 21400, 21800, 21525, 5},
-               {"11m", 25600, 27500, 27220, 1}};
+Band band[] = {{" MW", 520, 1700, 810, 10, 0},
+               {"90m", 3200,3500, 3300, 5, 0},  
+               {"75m", 3900,4500, 4100, 5, 0},  
+               {"60m", 4700, 5200, 4850, 5, 1},
+               {"49m", 5700, 6200, 6000, 5, 1},
+               {"41m", 7100, 7600, 7300, 5, 1},
+               {"31m", 9300, 10000, 9600, 5, 2},
+               {"25m", 11400, 12200, 11940, 5, 2},
+               {"22m", 13500, 13900, 13740, 5, 2},
+               {"19m", 15000, 15800, 15300, 5, 2},
+               {"16m", 17400, 17900, 17850, 5, 3},
+               {"13m", 21400, 21800, 21525, 5, 3},
+               {"11m", 25600, 27500, 27220, 1, 3}};
 
 const int lastBand = (sizeof band / sizeof(Band)) - 1;
-int currentFreqIdx = 3; // Default SW band is 31M
+int currentFreqIdx = 5; // Default SW band is 41M
 
 uint16_t currentFrequency;
 uint8_t bpfValue;
 
-AutoBPF bpf;            // Declare the Auto bandpass filter class.
+AutoBPF bpf; // Declare the Auto bandpass filter class.
 
 SI4735 si4735;
 
 void setup()
 {
   Serial.begin(9600);
-  while (!Serial) {};
+  while (!Serial)
+  {
+  };
 
   Serial.println("\nTest and validation of the Band Pass Filter working with the SI473X based receiver.\n");
 
-  si4735.setAudioMuteMcuPin(AUDIO_MUTE);    // avoiding pop in the speaker
+  si4735.setAudioMuteMcuPin(AUDIO_MUTE); // avoiding pop in the speaker
 
   bpf.setup(FILTER_PIN1, FILTER_PIN2);
-  bpf.setFilter(0);
-
-
 
   // gets and sets the Si47XX I2C bus address.
   int16_t si4735Addr = si4735.getDeviceI2CAddress(RESET_PIN);
@@ -89,13 +88,8 @@ void setup()
     Serial.println(si4735Addr, HEX);
   }
 
-
-  delay(500);
   si4735.setup(RESET_PIN, AM_FUNCTION);
-
-  // Starts defaul radio function and band (FM; from 84 to 108 MHz; 103.9 MHz; step 100kHz)
-  si4735.setAM(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep);
-
+  setBand(); // Switches to the default band (check currentFreqIdx)
   delay(500);
 
   currentFrequency = si4735.getFrequency();
@@ -104,11 +98,11 @@ void setup()
   showStatus();
 }
 
-void showHelp() {
+void showHelp()
+{
   Serial.println("Type U to increase and D to decrease the frequency");
-  Serial.println("Type S or s to seek station Up or Down");
   Serial.println("Type + or - to volume Up or Down");
-  Serial.println("Type X to show current status");
+  Serial.println("Type S to show current status");
   Serial.println("Type W go direct to MW/AM");
   Serial.println("Type > to go to the next SW band");
   Serial.println("Type < to go to the previous SW band");
@@ -120,16 +114,12 @@ void showHelp() {
 // Show current frequency and status
 void showStatus()
 {
-
   delay(250);
   band[currentFreqIdx].currentFreq = currentFrequency = si4735.getFrequency();
-
   Serial.print("You are tuned on ");
-
   Serial.println("Band: ");
   Serial.println(band[currentFreqIdx].freqName);
-  
-  
+
   Serial.print(currentFrequency);
   Serial.print("kHz");
   si4735.getCurrentReceivedSignalQuality();
@@ -140,11 +130,20 @@ void showStatus()
   Serial.print(" RSSI:");
   Serial.print(si4735.getCurrentRSSI());
   Serial.println("dBuV]");
-   
+
   Serial.print("Current BPF: ");
   Serial.println(bpfValue);
 }
 
+/**
+ * Switches the band and set the right filter as well.  
+ */
+void setBand() {
+  si4735.setAM(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep);
+  bpf.setFilter(bpfValue = band[currentFreqIdx].filter); // Sets the right filter to the band
+  delay(50);
+  currentFrequency = band[currentFreqIdx].currentFreq;
+}
 
 void loop()
 {
@@ -159,7 +158,6 @@ void loop()
     case '-':
       si4735.volumeDown();
       break;
-      break;
     case '>':
       if (currentFreqIdx < lastBand)
       {
@@ -169,11 +167,7 @@ void loop()
       {
         currentFreqIdx = 0;
       }
-      si4735.setAM(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep);
-      si4735.setAvcAmMaxGain(48); // Sets the maximum gain for automatic volume control on AM mode
-
-      delay(100);
-      currentFrequency = band[currentFreqIdx].currentFreq;
+      setBand();
       showStatus();
       break;
     case '<':
@@ -185,17 +179,13 @@ void loop()
       {
         currentFreqIdx = lastBand;
       }
-      si4735.setAM(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep);
-      delay(100);
-      currentFrequency = band[currentFreqIdx].currentFreq;
+      setBand();
       showStatus();
       break;
     case 'W':
     case 'w':
       currentFreqIdx = 0;
-      si4735.setAM(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep);
-      delay(100);
-      currentFrequency = band[currentFreqIdx].currentFreq;
+      setBand();
       showStatus();
       break;
     case 'U':
@@ -209,14 +199,6 @@ void loop()
       showStatus();
       break;
     case 'S':
-      si4735.seekStationUp();
-      showStatus();
-      break;
-    case 's':
-      si4735.seekStationDown();
-      showStatus();
-      break;
-    case 'X':
       showStatus();
       break;
     case '?':
@@ -228,7 +210,7 @@ void loop()
     case '3':
       bpfValue = key - '0'; // Converts char digit number to integer value.
       Serial.print("\nYou selected the BPF ");
-      Serial.print(bpfValue);  
+      Serial.print(bpfValue);
       bpf.setFilter(bpfValue);
       Serial.print("\n\nCheck the receiver...\n\n");
       showStatus();
