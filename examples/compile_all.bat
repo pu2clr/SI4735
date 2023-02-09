6eefc7479045a60ab15ecbf06450d7b0fc132d36
Ricardo <pu2clr@gmail.com>
2023-02-09 03:35:11 -0300

    Examples

--- examples/compile_all.bat
+++ examples/compile_all.bat
@@ -15,3 +15,23 @@ arduino-cli compile -b lgt8fx:avr:328  .\SI47XX_09_NOKIA_5110\ALL_IN_ONE_ONE_ENC
 
 arduino-cli compile -b arduino:avr:nano .\SI47XX_04_TFT\SI47XX_01_TFT_ILI9225
 Rem arduino-cli compile -b  lgt8fx:avr:328 .\SI47XX_04_TFT\SI47XX_01_TFT_ILI9225
+
+
+
+arduino-cli compile -b MightyCore:avr:32:bootloader=no_bootloader,LTO=Os_flto,clock=16MHz_external  .\SI47XX_17_ATMEGA32\OLED_ALL_IN_ONE 
+
+arduino-cli compile -b MegaCore:avr:128:bootloader=no_bootloader,LTO=Os_flto .\SI47XX_17_ATMEGA128\OLED_ALL_IN_ONE  
+
+
+arduino-cli compile --fqbn ATTinyCore:avr:attinyx5  .\SI47XX_05_ATTINY85\SI47XX_02_ATTINY85_MINI_OLED_I2C
+
+
+arduino-cli compile --fqbn esp8266:esp8266:generic  .\SI47XX_06_ESP8266\OLED_ALL_IN_ONE 
+arduino-cli compile --fqbn esp32:esp32:esp32-poe-iso .\SI47XX_06_ESP32\LCD16x2_ALL_IN_ONE
+
+arduino-cli compile --fqbn Seeeduino:samd:seeed_XIAO_m0  .\SI47XX_15_SEEEDUINO\OLED_ALL_IN_ONE
+
+arduino-cli compile --fqbn stm32duino:STM32F1:genericSTM32F103C .\SI47XX_07_STM32\STM32_04_OLED_ALL_IN_ONE_V2
+
+arduino-cli compile --fqbn arduino:sam:arduino_due_x .\SI47XX_16_ARDUINO_DUE_MEGA\SI47XX_02_RDS_TFT_TOUCH_SHIELD_35_V2
+arduino-cli compile --fqbn arduino:avr:mega .\SI47XX_16_ARDUINO_DUE_MEGA\SI47XX_02_RDS_TFT_TOUCH_SHIELD_35_V2 
