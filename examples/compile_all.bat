56ffaaa75b80502ab42619a05d68afbae1ae1b38
Ricardo Lima Caratti <pu2clr@gmail.com>
2023-02-09 10:40:56 -0300

    Examples

--- examples/compile_all.bat
+++ examples/compile_all.bat
@@ -2,22 +2,22 @@
 Rem Teste
 
 ECHO "ATmega and LGTF8X based boards"
-arduino-cli compile -b arduino:avr:nano .\SI47XX_01_SERIAL_MONITOR\SI4735_01_POC --output-dir %homepath%/Downloads/hex/atmega/SI4735_01_POC
-arduino-cli compile -b lgt8fx:avr:328 .\SI47XX_01_SERIAL_MONITOR\SI4735_01_POC --output-dir %homepath%/Downloads/hex/lgt8fx/SI4735_01_POC
-arduino-cli compile -b arduino:avr:nano .\SI47XX_02_for_Mirko_Pavleski_radio\MIRKO_V2 --output-dir %homepath%/Downloads/hex/atmega/MIRKO_V2
-arduino-cli compile -b lgt8fx:avr:328   .\SI47XX_02_for_Mirko_Pavleski_radio\MIRKO_V2 --output-dir %homepath%/Downloads/hex/lgt8fx/MIRKO_V2
-arduino-cli compile -b arduino:avr:nano .\SI47XX_03_OLED_I2C\SI47XX_02_ALL_IN_ONE_OLED --output-dir %homepath%/Downloads/hex/atmega/SI47XX_02_ALL_IN_ONE_OLED
-arduino-cli compile -b arduino:avr:pro .\SI47XX_09_NOKIA_5110\ALL_IN_ONE_ONE_ENCODER --output-dir %homepath%/Downloads/hex/atmega/SI47XX_09_NOKIA_5110
-arduino-cli compile -b lgt8fx:avr:328  .\SI47XX_09_NOKIA_5110\ALL_IN_ONE_ONE_ENCODER --output-dir %homepath%/Downloads/hex/lgt8fx/SI47XX_09_NOKIA_5110
-arduino-cli compile -b arduino:avr:nano .\SI47XX_04_TFT\SI47XX_01_TFT_ILI9225 --output-dir %homepath%/Downloads/hex/atmega/SI47XX_01_TFT_ILI9225
+arduino-cli compile -b arduino:avr:nano .\SI47XX_01_SERIAL_MONITOR\SI4735_01_POC --output-dir %homepath%\Downloads\hex\atmega\SI4735_01_POC
+arduino-cli compile -b lgt8fx:avr:328 .\SI47XX_01_SERIAL_MONITOR\SI4735_01_POC --output-dir %homepath%\Downloads\hex\lgt8fx\SI4735_01_POC
+arduino-cli compile -b arduino:avr:nano .\SI47XX_02_for_Mirko_Pavleski_radio\MIRKO_V2 --output-dir %homepath%\Downloads\hex\atmega\MIRKO_V2
+arduino-cli compile -b lgt8fx:avr:328   .\SI47XX_02_for_Mirko_Pavleski_radio\MIRKO_V2 --output-dir %homepath%\Downloads\hex\lgt8fx\MIRKO_V2
+arduino-cli compile -b arduino:avr:nano .\SI47XX_03_OLED_I2C\SI47XX_02_ALL_IN_ONE_OLED --output-dir %homepath%\Downloads\hex\atmega\SI47XX_02_ALL_IN_ONE_OLED
+arduino-cli compile -b arduino:avr:pro .\SI47XX_09_NOKIA_5110\ALL_IN_ONE_ONE_ENCODER --output-dir %homepath%\Downloads\hex\atmega\SI47XX_09_NOKIA_5110
+arduino-cli compile -b lgt8fx:avr:328  .\SI47XX_09_NOKIA_5110\ALL_IN_ONE_ONE_ENCODER --output-dir %homepath%\Downloads\hex\lgt8fx\SI47XX_09_NOKIA_5110
+arduino-cli compile -b arduino:avr:nano .\SI47XX_04_TFT\SI47XX_01_TFT_ILI9225 --output-dir %homepath%\Downloads\hex\atmega\SI47XX_01_TFT_ILI9225
 
 
 ECHO "ATmega 32 and 128 based boards"
-arduino-cli compile -b MightyCore:avr:32:bootloader=no_bootloader,LTO=Os_flto,clock=16MHz_external  .\SI47XX_17_ATMEGA32\OLED_ALL_IN_ONE --output-dir %homepath%/Downloads/hex/atmega32/OLED_ALL_IN_ONE
-arduino-cli compile -b MegaCore:avr:128:bootloader=no_bootloader,LTO=Os_flto .\SI47XX_17_ATMEGA128\OLED_ALL_IN_ONE --output-dir %homepath%/Downloads/hex/atmega128/OLED_ALL_IN_ONE 
+arduino-cli compile -b MightyCore:avr:32:bootloader=no_bootloader,LTO=Os_flto,clock=16MHz_external  .\SI47XX_17_ATMEGA32\OLED_ALL_IN_ONE --output-dir %homepath%\Downloads\hex\atmega32\OLED_ALL_IN_ONE
+arduino-cli compile -b MegaCore:avr:128:bootloader=no_bootloader,LTO=Os_flto .\SI47XX_17_ATMEGA128\OLED_ALL_IN_ONE --output-dir %homepath%\Downloads\hex\atmega128\OLED_ALL_IN_ONE 
 
 ECHO "ATtiny Core"
-arduino-cli compile --fqbn ATTinyCore:avr:attinyx5  .\SI47XX_05_ATTINY85\SI47XX_02_ATTINY85_MINI_OLED_I2C  %homepath%/Downloads/hex/attiny/SI47XX_02_ATTINY85_MINI_OLED_I2C 
+arduino-cli compile --fqbn ATTinyCore:avr:attinyx5  .\SI47XX_05_ATTINY85\SI47XX_02_ATTINY85_MINI_OLED_I2C  %homepath%\Downloads\hex\attiny\SI47XX_02_ATTINY85_MINI_OLED_I2C 
 
 
 
