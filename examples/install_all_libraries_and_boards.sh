3cb5d92c20997574470abea73fddffeb21581fe5
pu2clr <pu2clr@gmail.com>
2023-02-13 13:04:10 -0300

    Examples

---
+++ examples/install_all_libraries_and_boards.sh
@@ -0,0 +1,20 @@
+arduino-cli lib install "PU2CLR SI4735"
+arduino-cli lib install "Adafruit BusIO"
+arduino-cli lib install "Adafruit SH110X"
+arduino-cli lib install "Adafruit SSD1306"
+arduino-cli lib install "Adafruit TouchScreen"
+arduino-cli lib install "ES32Lab"
+arduino-cli lib install "Etherkit Si5351"
+arduino-cli lib install "FlashStorage_SAMD"
+arduino-cli lib install "LiquidCrystal"
+arduino-cli lib install "LiquidCrystal I2C"
+arduino-cli lib install "MCUFRIEND_kbv"
+arduino-cli lib install "TFT_22_ILI9225"
+arduino-cli lib install "TFT_eSPI_ES32Lab"
+arduino-cli lib install "Tiny4kOLED"
+arduino-cli lib install "TinyOLED-Fonts"
+# Nokia 5110 - The library LCD5110_Graph has to be installed manually. See: http://www.RinkyDinkElectronics.com/
+# The "Adafruit_SH1106" has to be installed manually. See: https://github.com/wonho-maker/Adafruit_SH1106
+
+  
+
