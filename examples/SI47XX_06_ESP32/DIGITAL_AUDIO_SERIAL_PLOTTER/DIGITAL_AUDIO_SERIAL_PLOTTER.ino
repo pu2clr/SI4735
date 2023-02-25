0f907c50e8104bdb14c63a40947ec29a1d2b8297
pu2clr <pu2clr@gmail.com>
2023-02-25 08:56:16 -0300

    Examples

--- examples/SI47XX_06_ESP32/DIGITAL_AUDIO_SERIAL_PLOTTER/DIGITAL_AUDIO_SERIAL_PLOTTER.ino
+++ examples/SI47XX_06_ESP32/DIGITAL_AUDIO_SERIAL_PLOTTER/DIGITAL_AUDIO_SERIAL_PLOTTER.ino
@@ -88,7 +88,7 @@ SI4735 rx;
 #define I2S_SCK 33
 
 #define I2C_SDA 21
-#define I2C_CLK 22 
+#define I2C_CLK 22
 
 // Define input buffer length
 #define bufferLen 64
@@ -99,15 +99,15 @@ int16_t sBuffer[bufferLen];
 uint16_t currentFrequency;
 uint16_t previousFrequency;
 uint8_t bandwidthIdx = 0;
-const char *bandwidth[] = {"6", "4", "3", "2", "1", "1.8", "2.5"};
+const char *bandwidth[] = { "6", "4", "3", "2", "1", "1.8", "2.5" };
 
 
 
 const i2s_config_t i2s_config = {
   .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
   .sample_rate = 48000,
-  .bits_per_sample = i2s_bits_per_sample_t(16),  // Resolution: More bits better quality  
-  .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT, 
+  .bits_per_sample = i2s_bits_per_sample_t(16),  // Resolution: More bits better quality
+  .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
   .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
   .intr_alloc_flags = 0,
   .dma_buf_count = 8,
@@ -123,8 +123,7 @@ const i2s_pin_config_t pin_config = {
 };
 
 
-void showHelp()
-{
+void showHelp() {
   Serial.println("Type F to FM; A to MW; L to LW; and 1 to SW");
   Serial.println("Type U to increase and D to decrease the frequency");
   Serial.println("Type S or s to seek station Up or Down");
@@ -137,19 +136,16 @@ void showHelp()
 }
 
 
-void showStatus()
-{
+void showStatus() {
+  currentFrequency = rx.getFrequency();
   rx.getStatus();
   rx.getCurrentReceivedSignalQuality();
   Serial.print("You are tuned on ");
-  if (rx.isCurrentTuneFM())
-  {
+  if (rx.isCurrentTuneFM()) {
     Serial.print(String(currentFrequency / 100.0, 2));
     Serial.print("MHz ");
     Serial.print((rx.getCurrentPilot()) ? "STEREO" : "MONO");
-  }
-  else
-  {
+  } else {
     Serial.print(currentFrequency);
     Serial.print("kHz");
   }
@@ -165,164 +161,162 @@ void showStatus()
 
 void setup() {
   Serial.begin(115200);
-  while(!Serial);
+  while (!Serial)
+    ;
 
   digitalWrite(RESET_PIN, HIGH);
-  
-  Wire.begin(I2C_SDA,I2C_CLK);
+
+  Wire.begin(I2C_SDA, I2C_CLK);
 
   delay(500);
-  Serial.println("\nrx.setup..."); 
+  Serial.println("\nrx.setup...");
   Serial.flush();
 
   // Sets active 32.768kHz crystal (32768Hz)
-  rx.setRefClock(32768);           // Ref = 32768Hz
-  rx.setRefClockPrescaler(1);      // 32768 x 1 = 32768Hz
+  rx.setRefClock(32768);       // Ref = 32768Hz
+  rx.setRefClockPrescaler(1);  // 32768 x 1 = 32768Hz
 
   // Use SI473X_DIGITAL_AUDIO1       - Digital audio output (SI4735 device pins: 3/DCLK, 24/LOUT/DFS, 23/ROUT/DIO )
   // Use SI473X_DIGITAL_AUDIO2       - Digital audio output (SI4735 device pins: 3/DCLK, 2/DFS, 1/DIO)
   // Use SI473X_ANALOG_DIGITAL_AUDIO - Analog and digital audio outputs (24/LOUT/ 23/ROUT and 3/DCLK, 2/DFS, 1/DIO)
   // XOSCEN_RCLK                     - Use external source clock (active crystal or signal generator)
-  rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_ANALOG_DIGITAL_AUDIO, XOSCEN_RCLK); // Analog and digital audio outputs (LOUT/ROUT and DCLK, DFS, DIO), external RCLK
-  // rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_DIGITAL_AUDIO2, XOSCEN_RCLK); 
+  rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_ANALOG_DIGITAL_AUDIO, XOSCEN_RCLK);  // Analog and digital audio outputs (LOUT/ROUT and DCLK, DFS, DIO), external RCLK
+  // rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_DIGITAL_AUDIO2, XOSCEN_RCLK);
   Serial.println("SI473X device started with Digital Audio setup!");
   delay(1000);
-  rx.setFM(8400, 10800, 10650, 10); // frequency station 10650 (106.50 MHz)
+  rx.setFM(8400, 10800, 10650, 10);  // frequency station 10650 (106.50 MHz)
+  // rx.setAM(570, 1710, 810, 10);
   delay(500);
-  Serial.print("\nrx.getFrequency: "); 
+  Serial.print("\nrx.getFrequency: ");
   Serial.println(rx.getFrequency());
   Serial.flush();
   delay(2000);
-  Serial.print("\nThe current frequency is: "); 
+  Serial.print("\nThe current frequency is: ");
   Serial.println(rx.getFrequency());
   Serial.flush();
   delay(2000);
   rx.setVolume(40);
 
-  Serial.print("\nSetting SI473X Sample rate to 48K."); 
-  rx.digitalOutputSampleRate(48000); 
+  Serial.print("\nSetting SI473X Sample rate to 48K.");
+  rx.digitalOutputSampleRate(48000);
 
   delay(2000);
 
   // OSIZE Dgital Output Audio Sample Precision (0=16 bits, 1=20 bits, 2=24 bits, 3=8bits).
   // OMONO Digital Output Mono Mode (0=Use mono/stereo blend ).
   // OMODE Digital Output Mode (0=I2S, 6 = Left-justified, 8 = MSB at second DCLK after DFS pulse, 12 = MSB at first DCLK after DFS pulse).
-  // OFALL Digital Output DCLK Edge (0 = use DCLK rising edge, 1 = use DCLK falling edge) 
-  rx.digitalOutputFormat(0 /* OSIZE */, 0 /* OMONO */, 0 /* OMODE */, 0/* OFALL*/);
+  // OFALL Digital Output DCLK Edge (0 = use DCLK rising edge, 1 = use DCLK falling edge)
+  rx.digitalOutputFormat(0 /* OSIZE */, 0 /* OMONO */, 0 /* OMODE */, 0 /* OFALL*/);
 
-  Serial.print("\nSI473X device is setted to digital Audio."); 
+  Serial.print("\nSI473X device is setted to digital Audio.");
 
   delay(2000);
 
-  Serial.print("\nSetting ESP32 I2S."); 
+  Serial.print("\nSetting ESP32 I2S.");
 
   i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
   i2s_set_pin(I2S_NUM_0, &pin_config);
   i2s_start(I2S_NUM_0);
 
-  Serial.print("\nI2S setup is done!. Now you can open the Serial Plot Monitor."); 
+  Serial.print("\nI2S setup is done!. Now you can open the Serial Plot Monitor.");
   delay(2000);
 
   showHelp();
   showStatus();
- 
 }
 
 void loop() {
 
-  // False print statements to "lock range" on serial plotter display
-  // Change rangelimit value to adjust "sensitivity"
-  int rangelimit = 3000;
-  Serial.print(rangelimit * -1);
-  Serial.print(" ");
-  Serial.print(rangelimit);
-  Serial.print(" ");
-
-
-  // Get I2S data and place in data buffer
-  size_t bytesIn = 0;
-  esp_err_t result = i2s_read(I2S_NUM_0, &sBuffer, bufferLen, &bytesIn, portMAX_DELAY);
-
-  if (result == ESP_OK)
-  {
-    // Read I2S data buffer
-    int16_t samples_read = bytesIn / 8;
-    if (samples_read > 0) {
-      float mean = 0;
-      for (int16_t i = 0; i < samples_read; ++i) {
-        mean += (sBuffer[i]);
-      }
-
-      // Average the data reading
-      mean /= samples_read;
-
-      // Print to serial plotter
-      Serial.println(mean);
-    }
-  }
-
-  if (Serial.available() > 0)
-  {
+  if (Serial.available() > 0) {
     char key = Serial.read();
-    switch (key)
-    {
-    case '+':
-      rx.volumeUp();
-      break;
-    case '-':
-      rx.volumeDown();
-      break;
-    case 'a':
-    case 'A':
-      rx.setAM(570, 1710, 810, 10);
-      break;
-    case 'f':
-    case 'F':
-      rx.setFM(8600, 10800, 10390, 10);
-      break;
-    case '1':
-      rx.setAM(9400, 9990, 9600, 5);
-      break;
-    case 'U':
-    case 'u':
-      rx.frequencyUp();
-      break;
-    case 'D':
-    case 'd':
-      rx.frequencyDown();
-      break;
-    case 'b':
-    case 'B':
-      if (rx.isCurrentTuneFM())
-      {
-        Serial.println("Not valid for FM");
+    switch (key) {
+      case '+':
+        rx.volumeUp();
+        break;
+      case '-':
+        rx.volumeDown();
+        break;
+      case 'a':
+      case 'A':
+        rx.setAM(570, 1710, 810, 10);
+        break;
+      case 'f':
+      case 'F':
+        rx.setFM(8600, 10800, 10650, 10);
+        break;
+      case '1':
+        rx.setAM(9400, 9990, 9600, 5);
+        break;
+      case 'U':
+      case 'u':
+        rx.frequencyUp();
+        showStatus();
+        delay(900);
+        break;
+      case 'D':
+      case 'd':
+        rx.frequencyDown();
+        showStatus();
+        delay(900);
+        break;
+      case 'b':
+      case 'B':
+        if (rx.isCurrentTuneFM()) {
+          Serial.println("Not valid for FM");
+        } else {
+          if (bandwidthIdx > 6)
+            bandwidthIdx = 0;
+          rx.setBandwidth(bandwidthIdx, 1);
+          Serial.print("Filter - Bandwidth: ");
+          Serial.print(String(bandwidth[bandwidthIdx]));
+          Serial.println(" kHz");
+          bandwidthIdx++;
+        }
+        break;
+      case 'S':
+        rx.seekStationUp();
+        break;
+      case 's':
+        rx.seekStationDown();
+        break;
+      case '0':
+        showStatus();
+        break;
+      case '?':
+        showHelp();
+        break;
+      default:
+        break;
+    }
+  } else {
+    // False print statements to "lock range" on serial plotter display
+    // Change rangelimit value to adjust "sensitivity"
+    int rangelimit = 3000;
+    Serial.print(rangelimit * -1);
+    Serial.print(" ");
+    Serial.print(rangelimit);
+    Serial.print(" ");
+
+
+    // Get I2S data and place in data buffer
+    size_t bytesIn = 0;
+    esp_err_t result = i2s_read(I2S_NUM_0, &sBuffer, bufferLen, &bytesIn, portMAX_DELAY);
+
+    if (result == ESP_OK) {
+      // Read I2S data buffer
+      int16_t samples_read = bytesIn / 8;
+      if (samples_read > 0) {
+        float mean = 0;
+        for (int16_t i = 0; i < samples_read; ++i) {
+          mean += (sBuffer[i]);
+        }
+
+        // Average the data reading
+        mean /= samples_read;
+
+        // Print to serial plotter
+        Serial.println(mean);
       }
-      else
-      {
-        if (bandwidthIdx > 6)
-          bandwidthIdx = 0;
-        rx.setBandwidth(bandwidthIdx, 1);
-        Serial.print("Filter - Bandwidth: ");
-        Serial.print(String(bandwidth[bandwidthIdx]));
-        Serial.println(" kHz");
-        bandwidthIdx++;
-      }
-      break;
-    case 'S':
-      rx.seekStationUp();
-      break;
-    case 's':
-      rx.seekStationDown();
-      break;
-    case '0':
-      showStatus();
-      break;
-    case '?':
-      showHelp();
-      break;
-    default:
-      break;
     }
   }
-
 }
