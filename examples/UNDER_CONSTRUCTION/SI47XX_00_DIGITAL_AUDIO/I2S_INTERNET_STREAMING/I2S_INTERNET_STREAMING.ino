d16a92015011fcf853ec49935f05173de5e9d415
Ricardo Lima Caratti <pu2clr@gmail.com>
2023-03-13 11:55:19 -0300

    Examples

--- examples/UNDER_CONSTRUCTION/SI47XX_00_DIGITAL_AUDIO/I2S_INTERNET_STREAMING/I2S_INTERNET_STREAMING.ino
+++ examples/UNDER_CONSTRUCTION/SI47XX_00_DIGITAL_AUDIO/I2S_INTERNET_STREAMING/I2S_INTERNET_STREAMING.ino
@@ -1,5 +1,7 @@
 /*
 
+  Under construction... 
+
   This sketch is based on the Graham's Mic UDP project: https://gist.github.com/GrahamM/1d5ded26b23f808a80520e8c1510713a
 
   The station received by the SI473X device will be streaming via Internet.
@@ -93,7 +95,7 @@ SI4735 rx;
 #define I2C_CLK 22
 
 // Define input buffer length
-#define bufferLen 64
+#define BUFFER_SIZE 64
 
 // WiFi connection - Network name and password
 const char *ssidName = "<WiFiName>";
@@ -103,39 +105,45 @@ const char *ssidPswd = "<WiFiPassword>";
 IPAddress udpAddress(192, 168, 0, 46);
 const int udpPort = 4735;
 
+boolean connected = false;
+
+const i2s_port_t I2S_PORT = I2S_NUM_0;
+const int BLOCK_SIZE = 128;
+
 // UDP
 AsyncUDP udp;
 
-int16_t sBuffer[bufferLen];
+int16_t stream_buffer[BUFFER_SIZE];
 
 uint16_t currentFrequency;
 uint16_t previousFrequency;
 uint8_t bandwidthIdx = 0;
-const char *bandwidth[] = {"6", "4", "3", "2", "1", "1.8", "2.5"};
+const char *bandwidth[] = { "6", "4", "3", "2", "1", "1.8", "2.5" };
 uint8_t currentVolume = 40;
 
 uint16_t amLastFrequency = 810;
 uint16_t fmLastFrequency = 10390;
 
 const i2s_config_t i2s_config = {
-    .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
-    .sample_rate = 48000,
-    .bits_per_sample = i2s_bits_per_sample_t(16), // Resolution: More bits better quality
-    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
-    .intr_alloc_flags = 0,
-    .dma_buf_count = 8,
-    .dma_buf_len = bufferLen,
-    .use_apll = false};
+  .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+  .sample_rate = 48000,
+  .bits_per_sample = i2s_bits_per_sample_t(16),  // Resolution: More bits better quality
+  .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+  .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
+  .intr_alloc_flags = 0,
+  .dma_buf_count = 8,
+  .dma_buf_len = BUFFER_SIZE,
+  .use_apll = false
+};
 
 const i2s_pin_config_t pin_config = {
-    .bck_io_num = I2S_SCK,
-    .ws_io_num = I2S_WS,
-    .data_out_num = -1,
-    .data_in_num = I2S_SD};
+  .bck_io_num = I2S_SCK,
+  .ws_io_num = I2S_WS,
+  .data_out_num = -1,
+  .data_in_num = I2S_SD
+};
 
-void showHelp()
-{
+void showHelp() {
   Serial.println("Type F to FM; A to MW; L to LW; and 1 to SW");
   Serial.println("Type U to increase and D to decrease the frequency");
   Serial.println("Type S or s to seek station Up or Down");
@@ -148,303 +156,214 @@ void showHelp()
 }
 
 // WiFi setup
-void setupWiFi()
-{
+void setupWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssidName, ssidPswd);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED)
-  {
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("WiFi Failed");
-    while (1)
-    {
+    while (1) {
       delay(1000);
     }
   }
 }
 
-int32_t buffer[512];        // Effectively two 1024 byte buffers
-volatile uint16_t rPtr = 0; // Yes, I should be using a pointer.
-
-void si473x_record()
-{
-  int num_bytes_read = i2s_read_bytes(I2S_PORT,
-                                      (char *)buffer + rPtr,
-                                      BLOCK_SIZE,     // Number of bytes
-                                      portMAX_DELAY); // No timeout
-
-  rPtr = rPtr + num_bytes_read;
-  // Wrap this when we get to the end of the buffer
-  if (rPtr > 2043)
-    rPtr = 0;
+void si473x_streaming() {
+  size_t bytesIn = 0;
+  esp_err_t result = i2s_read(I2S_NUM_0, &stream_buffer, BUFFER_SIZE, &bytesIn, portMAX_DELAY);
+  if (result == ESP_OK) {
+    if (!connected) {
+      if (udp.connect(udpAddress, udpPort)) {
+        connected = true;
+        Serial.println(" * Connected to host");
+      }
+    } else {
+      udp.write((uint8_t *)stream_buffer, sizeof(stream_buffer));
+    }
+  }
 }
 
-void showStatus()
-{
-  currentFrequency = rx.getFrequency();
-  rx.getStatus();
-  rx.getCurrentReceivedSignalQuality();
-  Serial.print("You are tuned on ");
-  if (rx.isCurrentTuneFM())
-  {
-    Serial.print(String(currentFrequency / 100.0, 2));
-    Serial.print("MHz ");
-    Serial.print((rx.getCurrentPilot()) ? "STEREO" : "MONO");
+  void showStatus() {
+    currentFrequency = rx.getFrequency();
+    rx.getStatus();
+    rx.getCurrentReceivedSignalQuality();
+    Serial.print("You are tuned on ");
+    if (rx.isCurrentTuneFM()) {
+      Serial.print(String(currentFrequency / 100.0, 2));
+      Serial.print("MHz ");
+      Serial.print((rx.getCurrentPilot()) ? "STEREO" : "MONO");
+    } else {
+      Serial.print(currentFrequency);
+      Serial.print("kHz");
+    }
+    Serial.print(" [SNR:");
+    Serial.print(rx.getCurrentSNR());
+    Serial.print("dB");
+
+    Serial.print(" Signal:");
+    Serial.print(rx.getCurrentRSSI());
+    Serial.println("dBuV");
+
+    Serial.print(" Volume:");
+    Serial.print(rx.getVolume());
+    Serial.println("]");
   }
-  else
-  {
-    Serial.print(currentFrequency);
-    Serial.print("kHz");
-  }
-  Serial.print(" [SNR:");
-  Serial.print(rx.getCurrentSNR());
-  Serial.print("dB");
 
-  Serial.print(" Signal:");
-  Serial.print(rx.getCurrentRSSI());
-  Serial.println("dBuV");
-
-  Serial.print(" Volume:");
-  Serial.print(rx.getVolume());
-  Serial.println("]");
-}
-
-/**
+  /**
  * Switch from FM to AM or AM to FM mode using Digital Audio Setup
  */
-void switchModeAmFm(uint16_t f)
-{
+  void switchModeAmFm(uint16_t f) {
 
-  if (rx.isCurrentTuneFM())
-  {
-    fmLastFrequency = currentFrequency;
-    rx.setup(RESET_PIN, -1, AM_CURRENT_MODE, SI473X_DIGITAL_AUDIO2, XOSCEN_RCLK);
-    rx.setAM(570, 1710, f, 10);
-    rx.digitalOutputSampleRate(48000);
-    rx.digitalOutputFormat(0, 0, 0, 0);
+    if (rx.isCurrentTuneFM()) {
+      fmLastFrequency = currentFrequency;
+      rx.setup(RESET_PIN, -1, AM_CURRENT_MODE, SI473X_DIGITAL_AUDIO2, XOSCEN_RCLK);
+      rx.setAM(570, 1710, f, 10);
+      rx.digitalOutputSampleRate(48000);
+      rx.digitalOutputFormat(0, 0, 0, 0);
+      rx.setVolume(currentVolume);
+    } else {
+      amLastFrequency = currentFrequency;
+      rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_DIGITAL_AUDIO2, XOSCEN_RCLK);
+      rx.setFM(8400, 10800, f, 10);
+      rx.digitalOutputSampleRate(48000);
+      rx.digitalOutputFormat(0, 0, 0, 0);
+      rx.setVolume(currentVolume);
+    }
+    showStatus();
+    delay(1000);
+  }
+
+  void setup() {
+    Serial.begin(115200);
+    while (!Serial)
+      ;
+
+    digitalWrite(RESET_PIN, HIGH);
+
+    Wire.begin(I2C_SDA, I2C_CLK);
+
+    delay(500);
+    Serial.println("\nrx.setup...");
+    Serial.flush();
+
+    // Sets active 32.768kHz crystal (32768Hz)
+    rx.setRefClock(32768);       // Ref = 32768Hz
+    rx.setRefClockPrescaler(1);  // 32768 x 1 = 32768Hz
+
+    // Use SI473X_DIGITAL_AUDIO1       - Digital audio output (SI4735 device pins: 3/DCLK, 24/LOUT/DFS, 23/ROUT/DIO )
+    // Use SI473X_DIGITAL_AUDIO2       - Digital audio output (SI4735 device pins: 3/DCLK, 2/DFS, 1/DIO)
+    // Use SI473X_ANALOG_DIGITAL_AUDIO - Analog and digital audio outputs (24/LOUT/ 23/ROUT and 3/DCLK, 2/DFS, 1/DIO)
+    // XOSCEN_RCLK                     - Use external source clock (active crystal or signal generator)
+    rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_ANALOG_DIGITAL_AUDIO, XOSCEN_RCLK);  // Analog and digital audio outputs (LOUT/ROUT and DCLK, DFS, DIO), external RCLK
+    // rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_DIGITAL_AUDIO2, XOSCEN_RCLK);
+    Serial.println("SI473X device started with Digital Audio setup!");
+    delay(1000);
+    rx.setFM(8400, 10800, 10650, 10);  // frequency station 10650 (106.50 MHz)
+    // rx.setAM(570, 1710, 810, 10);
+    delay(500);
+    Serial.print("\nrx.getFrequency: ");
+    Serial.println(rx.getFrequency());
+    Serial.flush();
+    delay(2000);
+    Serial.print("\nThe current frequency is: ");
+    Serial.println(rx.getFrequency());
+    Serial.flush();
+    delay(2000);
     rx.setVolume(currentVolume);
-  }
-  else
-  {
-    amLastFrequency = currentFrequency;
-    rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_DIGITAL_AUDIO2, XOSCEN_RCLK);
-    rx.setFM(8400, 10800, f, 10);
+
+    Serial.print("\nSetting SI473X Sample rate to 48K.");
     rx.digitalOutputSampleRate(48000);
-    rx.digitalOutputFormat(0, 0, 0, 0);
-    rx.setVolume(currentVolume);
-  }
-  showStatus();
-  delay(1000);
-}
 
-void setup()
-{
-  Serial.begin(115200);
-  while (!Serial)
-    ;
+    delay(2000);
 
-  digitalWrite(RESET_PIN, HIGH);
+    // OSIZE Dgital Output Audio Sample Precision (0=16 bits, 1=20 bits, 2=24 bits, 3=8bits).
+    // OMONO Digital Output Mono Mode (0=Use mono/stereo blend ).
+    // OMODE Digital Output Mode (0=I2S, 6 = Left-justified, 8 = MSB at second DCLK after DFS pulse, 12 = MSB at first DCLK after DFS pulse).
+    // OFALL Digital Output DCLK Edge (0 = use DCLK rising edge, 1 = use DCLK falling edge)
+    rx.digitalOutputFormat(0 /* OSIZE */, 0 /* OMONO */, 0 /* OMODE */, 0 /* OFALL*/);
 
-  Wire.begin(I2C_SDA, I2C_CLK);
+    Serial.print("\nSI473X device is setted to digital Audio.");
 
-  delay(500);
-  Serial.println("\nrx.setup...");
-  Serial.flush();
+    delay(2000);
 
-  // Sets active 32.768kHz crystal (32768Hz)
-  rx.setRefClock(32768);      // Ref = 32768Hz
-  rx.setRefClockPrescaler(1); // 32768 x 1 = 32768Hz
+    Serial.print("\nSetting ESP32 I2S.");
 
-  // Use SI473X_DIGITAL_AUDIO1       - Digital audio output (SI4735 device pins: 3/DCLK, 24/LOUT/DFS, 23/ROUT/DIO )
-  // Use SI473X_DIGITAL_AUDIO2       - Digital audio output (SI4735 device pins: 3/DCLK, 2/DFS, 1/DIO)
-  // Use SI473X_ANALOG_DIGITAL_AUDIO - Analog and digital audio outputs (24/LOUT/ 23/ROUT and 3/DCLK, 2/DFS, 1/DIO)
-  // XOSCEN_RCLK                     - Use external source clock (active crystal or signal generator)
-  rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_ANALOG_DIGITAL_AUDIO, XOSCEN_RCLK); // Analog and digital audio outputs (LOUT/ROUT and DCLK, DFS, DIO), external RCLK
-  // rx.setup(RESET_PIN, -1, FM_CURRENT_MODE, SI473X_DIGITAL_AUDIO2, XOSCEN_RCLK);
-  Serial.println("SI473X device started with Digital Audio setup!");
-  delay(1000);
-  rx.setFM(8400, 10800, 10650, 10); // frequency station 10650 (106.50 MHz)
-  // rx.setAM(570, 1710, 810, 10);
-  delay(500);
-  Serial.print("\nrx.getFrequency: ");
-  Serial.println(rx.getFrequency());
-  Serial.flush();
-  delay(2000);
-  Serial.print("\nThe current frequency is: ");
-  Serial.println(rx.getFrequency());
-  Serial.flush();
-  delay(2000);
-  rx.setVolume(currentVolume);
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM_0, &pin_config);
+    i2s_start(I2S_NUM_0);
 
-  Serial.print("\nSetting SI473X Sample rate to 48K.");
-  rx.digitalOutputSampleRate(48000);
+    Serial.print("\nI2S setup is done!");
 
-  delay(2000);
+    setupWiFi();
 
-  // OSIZE Dgital Output Audio Sample Precision (0=16 bits, 1=20 bits, 2=24 bits, 3=8bits).
-  // OMONO Digital Output Mono Mode (0=Use mono/stereo blend ).
-  // OMODE Digital Output Mode (0=I2S, 6 = Left-justified, 8 = MSB at second DCLK after DFS pulse, 12 = MSB at first DCLK after DFS pulse).
-  // OFALL Digital Output DCLK Edge (0 = use DCLK rising edge, 1 = use DCLK falling edge)
-  rx.digitalOutputFormat(0 /* OSIZE */, 0 /* OMONO */, 0 /* OMODE */, 0 /* OFALL*/);
+    delay(2000);
 
-  Serial.print("\nSI473X device is setted to digital Audio.");
-
-  delay(2000);
-
-  Serial.print("\nSetting ESP32 I2S.");
-
-  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-  i2s_set_pin(I2S_NUM_0, &pin_config);
-  i2s_start(I2S_NUM_0);
-
-  Serial.print("\nI2S setup is done!. Now you can open the Serial Plot Monitor.");
-  delay(2000);
-
-  showHelp();
-  showStatus();
-}
-
-void loop()
-{
-  static uint8_t state = 0;
-  si473x_record();
-
-  if (!connected)
-  {
-    if (udp.connect(udpAddress, udpPort))
-    {
-      connected = true;
-      Serial.println(" * Connected to host");
-    }
-  }
-  else
-  {
-    switch (state)
-    {
-    case 0: // wait for index to pass halfway
-      if (rPtr > 1023)
-      {
-        state = 1;
-      }
-      break;
-    case 1: // send the first half of the buffer
-      state = 2;
-      udp.write((uint8_t *)buffer, 1024);
-      break;
-    case 2: // wait for index to wrap
-      if (rPtr < 1023)
-      {
-        state = 3;
-      }
-      break;
-    case 3: // send second half of the buffer
-      state = 0;
-      udp.write((uint8_t *)buffer + 1024, 1024);
-      break;
-    }
+    showHelp();
+    showStatus();
   }
 
-  if (Serial.available() > 0)
-  {
-    char key = Serial.read();
-    switch (key)
-    {
-    case '+':
-      rx.setVolume(++currentVolume);
-      currentVolume = rx.getVolume();
-      break;
-    case '-':
-      rx.setVolume(--currentVolume);
-      currentVolume = rx.getVolume();
-      break;
-    case 'a':
-    case 'A':
-      switchModeAmFm(amLastFrequency);
-      break;
-    case 'f':
-    case 'F':
-      switchModeAmFm(fmLastFrequency);
-      break;
-    case 'U':
-    case 'u':
-      rx.frequencyUp();
-      showStatus();
-      delay(900);
-      break;
-    case 'D':
-    case 'd':
-      rx.frequencyDown();
-      showStatus();
-      delay(900);
-      break;
-    case 'b':
-    case 'B':
-      if (rx.isCurrentTuneFM())
-      {
-        Serial.println("Not valid for FM");
-      }
-      else
-      {
-        if (bandwidthIdx > 6)
-          bandwidthIdx = 0;
-        rx.setBandwidth(bandwidthIdx, 1);
-        Serial.print("Filter - Bandwidth: ");
-        Serial.print(String(bandwidth[bandwidthIdx]));
-        Serial.println(" kHz");
-        bandwidthIdx++;
-      }
-      break;
-    case 'S':
-      rx.seekStationUp();
-      break;
-    case 's':
-      rx.seekStationDown();
-      break;
-    case '0':
-      showStatus();
-      delay(1200);
-      break;
-    case '?':
-      showHelp();
-      break;
-    default:
-      break;
-    }
-  }
+  void loop() {
+    si473x_streaming();
 
-  /*
-  else
-  {
-    // False print statements to "lock range" on serial plotter display
-    // Change rangelimit value to adjust "sensitivity"
-    int rangelimit = 3000;
-    Serial.print(rangelimit * -1);
-    Serial.print(" ");
-    Serial.print(rangelimit);
-    Serial.print(" ");
-
-    // Get I2S data and place in data buffer
-    size_t bytesIn = 0;
-    esp_err_t result = i2s_read(I2S_NUM_0, &sBuffer, bufferLen, &bytesIn, portMAX_DELAY);
-
-    if (result == ESP_OK)
-    {
-      // Read I2S data buffer
-      int16_t samples_read = bytesIn / 8;
-      if (samples_read > 0)
-      {
-        float mean = 0;
-        for (int16_t i = 0; i < samples_read; ++i)
-        {
-          mean += (sBuffer[i]);
-        }
-
-        // Average the data reading
-        mean /= samples_read;
-
-        // Print to serial plotter
-        Serial.println(mean);
+    if (Serial.available() > 0) {
+      char key = Serial.read();
+      switch (key) {
+        case '+':
+          rx.setVolume(++currentVolume);
+          currentVolume = rx.getVolume();
+          break;
+        case '-':
+          rx.setVolume(--currentVolume);
+          currentVolume = rx.getVolume();
+          break;
+        case 'a':
+        case 'A':
+          switchModeAmFm(amLastFrequency);
+          break;
+        case 'f':
+        case 'F':
+          switchModeAmFm(fmLastFrequency);
+          break;
+        case 'U':
+        case 'u':
+          rx.frequencyUp();
+          showStatus();
+          delay(900);
+          break;
+        case 'D':
+        case 'd':
+          rx.frequencyDown();
+          showStatus();
+          delay(900);
+          break;
+        case 'b':
+        case 'B':
+          if (rx.isCurrentTuneFM()) {
+            Serial.println("Not valid for FM");
+          } else {
+            if (bandwidthIdx > 6)
+              bandwidthIdx = 0;
+            rx.setBandwidth(bandwidthIdx, 1);
+            Serial.print("Filter - Bandwidth: ");
+            Serial.print(String(bandwidth[bandwidthIdx]));
+            Serial.println(" kHz");
+            bandwidthIdx++;
+          }
+          break;
+        case 'S':
+          rx.seekStationUp();
+          break;
+        case 's':
+          rx.seekStationDown();
+          break;
+        case '0':
+          showStatus();
+          delay(1200);
+          break;
+        case '?':
+          showHelp();
+          break;
+        default:
+          break;
       }
     }
   }
-  */
-}
